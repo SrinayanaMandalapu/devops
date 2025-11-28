7dfd3b2c4afb724f929156b2f091a4281d78f198
SrinayanaMandalapu <mandalapunayana@gmail.com>
2025-11-28 13:42:58 +0530

    package-manager

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-import flask
-app = flask.Flask(__name__)
-@app.route('/')
-def home():
-    return "Welcome to the Package Manager!"
+import requests
+
+def get_status():
+    res = requests.get("https://api.github.com")
+    return res.status_code
+
+if __name__ == "__main__":
+    status = get_status()
+    print(f"GitHub API status code: {status}")
